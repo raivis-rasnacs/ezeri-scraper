06493eb9a4571ea2e48270aa76362273dbc5376b
Raivis <71266644+raivis-rasnacs@users.noreply.github.com>
2023-03-04 21:53:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import requests
-from requests.structures import CaseInsensitiveDict
 import bs4
-import random
 from pprint import pprint
 
 url = "https://lv.wikipedia.org/wiki/Latvijas_ezeru_uzskaitījums"
 
-""" data = requests.get(url)
+# Lasa datus no Wiki
+data = requests.get(url)
 html = bs4.BeautifulSoup(data.text, 'html.parser')
 
 tabula = html.select("table")[0]
 ezeri = tabula.select("tr")
+
+# Ieraksta datus failā
 with open("ezeri.txt", "a", encoding="UTF-8") as f:
     for ezers in ezeri[1:]:
         lauki = ezers.select("td")
-        f.write(lauki[2].text.strip()+"-"+lauki[6].text.strip()+"\n") """
+        f.write(lauki[2].text.strip()+"-"+lauki[6].text.strip()+"\n")
 
+# Ielasa datus no faila sarakstā
 ezeri = []
 with open("ezeri.txt", "r", encoding="utf-8") as f:
     for rinda in f:
@@ -25,10 +27,12 @@ with open("ezeri.txt", "r", encoding="utf-8") as f:
                     "Dziļums":float(rindas_dati[1].strip().replace(",", "."))}
             ezeri.append(ezers)
 
+# Burbuļkārto ezerus pēc dziļuma
 for i, ezers in enumerate(ezeri):
     for j, ezers in enumerate(ezeri):
         if j < len(ezeri) - 1:
             if ezeri[j]["Dziļums"] > ezeri[j + 1]["Dziļums"]:
                 ezeri[j], ezeri[j + 1] = ezeri[j + 1], ezeri[j]
 
+# Izdrukā sakārtotus datus
 pprint(ezeri)
